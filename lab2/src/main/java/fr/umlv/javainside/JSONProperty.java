8476dfb894b1921654ec21d1d954f2cf0800ce4f
Jeremy Thiebaud <jeremythiebaud98@gmail.com>
2020-09-29 16:15:52 +0200

    champs JSON en cache test fix JSONProperty

--- lab2/src/main/java/fr/umlv/javainside/JSONProperty.java
+++ lab2/src/main/java/fr/umlv/javainside/JSONProperty.java
@@ -4,9 +4,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.RecordComponent;
 
-@Target(ElementType.RECORD_COMPONENT)
+//@Target(ElementType.RECORD_COMPONENT)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JSONProperty {
     //String value();
