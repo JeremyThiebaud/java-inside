e5f584d60c123330d995dcc588c0a722922bc349
Jeremy Thiebaud <jeremythiebaud98@gmail.com>
2020-10-13 14:54:28 +0200

    update lab4

--- lab4/src/main/java/fr/umlv/javainside/Logger.java
+++ lab4/src/main/java/fr/umlv/javainside/Logger.java
@@ -3,6 +3,7 @@ package fr.umlv.javainside;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.lang.invoke.MutableCallSite;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -21,6 +22,17 @@ public interface Logger {
                 throw new AssertionError(e);
             }
         }
+
+        private static final ClassValue<MutableCallSite> ENABLE_CALLSITES = new ClassValue<>() {
+            protected MutableCallSite computeValue(Class<?> type) {
+                return new MutableCallSite(MethodHandles.constant(boolean.class, true));
+            }
+        };
+
+        public static void enable(Class<?> declaringClass, boolean enable) {
+            ENABLE_CALLSITES.get(declaringClass).setTarget(MethodHandles.constant(boolean.class, enable));
+        }
+
     }
 
     public void log(String message);
@@ -43,8 +55,50 @@ public interface Logger {
         };
     }
 
+    public static Logger lambdaOf(Class<?> declaringClass, Consumer<? super String> consumer) {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
+        var mh = createLoggingMethodHandle(declaringClass, consumer);
+        return (String message) -> {
+                try {
+                    mh.invokeExact(message);
+                } catch(RuntimeException | Error e) {
+                    throw e;
+                } catch(Throwable t) {
+                    throw new UndeclaredThrowableException(t);
+                }
+            };
+    }
+
+    public static Logger recordOf(Class<?> declaringClass, Consumer<? super String> consumer) {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(consumer);
+        var mh = createLoggingMethodHandle(declaringClass, consumer);
+        record loggerRecord(MethodHandle mh) implements Logger{
+            @Override
+            public void log(String message) {
+                try {
+                    mh.invokeExact(message);
+                } catch(RuntimeException | Error e) {
+                    throw e;
+                } catch(Throwable t) {
+                    throw new UndeclaredThrowableException(t);
+                }
+            }
+        }
+        return new loggerRecord(mh);
+    }
+
+//    private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
+//        var mh = Impl.ACCEPT.bindTo(consumer);
+//        return mh.asType(MethodType.methodType(void.class, String.class));
+//    }
     private static MethodHandle createLoggingMethodHandle(Class<?> declaringClass, Consumer<? super String> consumer) {
-        var mh = Impl.ACCEPT.bindTo(consumer);
-        return mh.asType(MethodType.methodType(void.class, String.class));
+        var mh = MethodHandles.guardWithTest(
+                Impl.ENABLE_CALLSITES.get(declaringClass).dynamicInvoker(),
+                Impl.ACCEPT.bindTo(consumer).asType(MethodType.methodType(void.class, String.class)),
+                MethodHandles.empty(MethodType.methodType(void.class, String.class))
+        );
+        return mh;
     }
 }
